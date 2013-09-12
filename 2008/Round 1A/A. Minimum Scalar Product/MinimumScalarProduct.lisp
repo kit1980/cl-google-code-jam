3071c3cfecb6cee14d8876e75a584748374bc5cc
Sergey Dymchenko <kit1980@gmail.com>
2013-09-11 17:25:20 -0700

    Add solution for '2008/Round 1A/A. Minimum Scalar Product'.

---
+++ 2008/Round 1A/A. Minimum Scalar Product/MinimumScalarProduct.lisp
@@ -0,0 +1,28 @@
+;; Google Code Jam
+;; Round 1A 2008
+;; A. Minimum Scalar Product
+;; https://code.google.com/codejam/contest/dashboard?c=32016#s=p0
+;;
+;; Algorithmic solution in Common Lisp.
+;; Author: Sergey Dymchenko <kit1980@gmail.com>
+;;
+;; SBCL 1.0.29.11 - http://www.sbcl.org/
+;; Usage:
+;; sbcl --load MinimumScalarProduct.lisp --eval "(sb-ext:save-lisp-and-die \"MinimumScalarProduct_lisp\" :executable t :toplevel 'main)" > /dev/null && ./MinimumScalarProduct_lisp < in-file > out-file
+
+(defun dot-product (x y)
+  (reduce '+ (mapcar '* x y)) )
+
+(defun min-product (x y)
+  (dot-product (sort x #'<) (sort y #'>)) )
+
+(defun do-case (case-num x y)
+  (format t "Case #~a: ~a~%" case-num (min-product x y)) )
+
+(defun main ()
+  (loop for case-num from 1 to (read) do
+       (let (n x y) 
+         (setf n (read))
+         (setf x (loop repeat n collect (read)))
+         (setf y (loop repeat n collect (read)))
+         (do-case case-num x y) ) ) )
